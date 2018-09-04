d7058e1c8265820f982fbe355b5446868bcdf2fc
Zac Colley <zac@farewill.com>
2018-09-04 22:48:41 +0100

    Update extend-expect.js

--- extend-expect.js
+++ extend-expect.js
@@ -1,6 +1,6 @@
 /* 
 
-This allows users to add `require('jest-axe/extend-expect')
+This allows users to add `require('jest-axe/extend-expect')`
 at the top of their test file rather than have two lines for this.
 
 It also allows users to use jest's setupFiles configuration and
