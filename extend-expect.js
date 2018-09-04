cb2dfae0d7d174e02813c4e78674b77bed9439d1
Zac Colley <hi@zaccolley.com>
2018-09-04 22:47:58 +0100

    Remove the word 'simply' from extend-expect.js file

--- extend-expect.js
+++ extend-expect.js
@@ -1,7 +1,13 @@
-// this allows users to simply add `require('jest-axe/extend-expect')`
-// at the top of their test file rather than have two lines for this.
-// it also allows users to use jest's setupFiles configuration and
-// point directly to `jest-axe/extend-expect`
+/* 
+
+This allows users to add `require('jest-axe/extend-expect')
+at the top of their test file rather than have two lines for this.
+
+It also allows users to use jest's setupFiles configuration and
+point directly to `jest-axe/extend-expect`
+
+*/
+
 const { toHaveNoViolations } = require('jest-axe')
 
 expect.extend(toHaveNoViolations)
