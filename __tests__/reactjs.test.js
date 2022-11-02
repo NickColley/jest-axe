90a0650f77a5d90d737efe7bca322e6775f2ceea
Nick Colley <nick.colley@hippodigital.co.uk>
2022-11-02 12:46:50 +0000

    Fix tests

--- __tests__/reactjs.test.js
+++ __tests__/reactjs.test.js
@@ -1,7 +1,8 @@
-const { axe, toHaveNoViolations } = require('../index')
-
 const React = require('react')
 const ReactDOMServer = require('react-dom/server')
+const { render } = require('@testing-library/react')
+
+const { axe, toHaveNoViolations } = require('../index')
 
 expect.extend(toHaveNoViolations)
 
@@ -18,8 +19,6 @@ describe('React', () => {
   })
 
   test('renders a react testing library container correctly', async () => {
-    const { render } = require('@testing-library/react')
-
     const element = React.createElement('img', { src: '#' })
     const { container } = render(element)
     const results = await axe(container)
@@ -30,8 +29,6 @@ describe('React', () => {
   })
 
   test('renders a react testing library container without duplicate ids', async () => {
-    const { render } = require('@testing-library/react')
-
     const element = React.createElement('img', { src: '#', alt: 'test', id: 'test' })
     const { container } = render(element)
     const results = await axe(container)
--- __tests__/vuejs.test.js
+++ __tests__/vuejs.test.js
@@ -1,3 +1,6 @@
+const { mount } = require('@vue/test-utils')
+const { render } = require('@testing-library/vue')
+
 const { axe, toHaveNoViolations } = require('../index')
 
 const Image = {
@@ -9,7 +12,6 @@ expect.extend(toHaveNoViolations)
 
 describe('Vue', () => {
   it('renders correctly', async () => {
-    const { mount } = require('@vue/test-utils')
     const wrapper = mount(Image)
     const results = await axe(wrapper.element)
     
@@ -19,7 +21,6 @@ describe('Vue', () => {
   })
 
   it('renders a vue testing library container correctly', async () => {
-    const { render } = require('@testing-library/vue')
     const { container } = render(Image)
     const results = await axe(container)
     
